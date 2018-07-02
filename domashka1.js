03b3ee6ae7182e58c3bb0814f9fb86829437da3d
Alexander <Alexander@MacBook-PRO.local>
2018-07-02 06:16:52 +0500

    na proverku

--- domashka1.js
+++ domashka1.js
@@ -1,85 +1,84 @@
-// const fse = require('fs-extra');
-// const path = require('path');
-// const arrayUniq = require('array-uniq');
-const commandLine = require('commander');
+const fs = require('fs');
+const fse = require('fs-extra');
+const path = require('path');
+const arrayUniq = require('array-uniq');
+const program = require('commander');
 
-commandLine
-  .version('0.0.1')
-  .option('-m, --mkdir [name]', 'Make a new directory in current path witn name [result]', 'result')
-  .option('-t, --targetdir [name]', 'Target directory for necessary sort files with name [input]', 'input')
+program
+  .version('1.0.0')
+  .option('-o, --outdir [name]', 'Directory with sorted files witn name [output]', 'output')
+  .option('-i, --inpdir [name]', 'Directory for sorting files with name [input]', 'input')
   .parse(process.argv);
 
-console.log(commandLine.mkdir);
-console.log(commandLine.targetdir);
 
-// const targetDir = './' + commandLine.targetdir;
-// const resultDir = './' + commandLine.mkdir;
-// let dirNames = [];
+const inputDir = './' + program.inpdir;
+const outputDir = './' + program.outdir;
+let dirNames = [];
 
-// const createNewDir = (directoryName) => {
-//   // если папка создана, то почистим внутри данной директории
-//   // если нет, то создадим
-//   if (fse.ensureDirSync(resultDir)) {
-//     console.log('есть');
-//     fse.removeSync(directoryName);
+const createNewDir = (directoryName) => {
+  // Creating output folder(if exist delete)
+  //if (fse.ensureDirSync(directoryName)) {
+    if (fs.existsSync(directoryName)) {
+    console.log('Folder exist');
+    fse.removeSync(directoryName);
 
-//     fse.mkdir(directoryName, (err) => {
-//       if (err) {
-//         console.log('Error');
-//       }
-//       console.log('Папка очищена');
-//     });
-//   } else {
-//     fse.mkdir(directoryName, (err) => {
-//       if (err) {
-//         console.log('Error');
-//       }
-//       console.log('Папка создана');
-//     });
-//   }
-// };
+    fs.mkdir(directoryName, (err) => {
+      if (err) {
+        console.log('Error');
+      }
+      console.log('Folder is empty');
+    });
+  } else {
+    fs.mkdir(directoryName, (err) => {
+      if (err) {
+        console.log('Error');
+      }
+      console.log('Folder created');
+    });
+  }
+};
 
-// // Прочитаем все файлы внутри нашей директории и запишем первые буквы в массив
-// const readDir = (targetDir, level) => {
-//   const files = fse.readdirSync(targetDir);
+// Creating catalog tree
+const readDir = (inputDir, level) => {
+  const files = fs.readdirSync(inputDir);
 
-//   files.forEach((item) => {
-//     let localBase = path.join(targetDir, item);
-//     let state = fse.statSync(localBase);
-//     if (state.isDirectory()) {
-//       readDir(localBase, level + 1);
-//     } else {
-//       dirNames.push(item[0]);
-//     }
-//   });
-// };
+  files.forEach((item) => {
+    let localBase = path.join(inputDir, item);
+    let state = fse.statSync(localBase);
+    if (state.isDirectory()) {
+      readDir(localBase, level + 1);
+    } else {
+      if (item[0]!='.') dirNames.push(item[0]);
+    }
+  });
+};
 
-// // Создадим папки в новой директории
-// const createDir = (directoriesNames) => {
-//   let uniqLetter = arrayUniq(directoriesNames);
+// /making folders in output
+const createDir = (directoriesNames) => {
+  let uniqLetter = arrayUniq(directoriesNames);
 
-//   uniqLetter.forEach((item, i, arr) => {
-//     fse.mkdirSync(path.join(resultDir, item));
-//   });
-// };
+  uniqLetter.forEach((item, i, arr) => {
+    console.log(path.join(outputDir, item));
+    fs.mkdirSync(path.join(outputDir, item));
+  });
+};
 
-// // Скопируем файлы
-// const copyFiles = (targetDir, resultDir) => {
-//   const files = fse.readdirSync(targetDir);
+// Copying files
+const copyFiles = (inputDir, outputDir) => {
+  const files = fs.readdirSync(inputDir);
 
-//   files.forEach((item) => {
-//     let localBase = path.join(targetDir, item);
-//     let state = fse.statSync(localBase);
-//     if (state.isDirectory()) {
-//       copyFiles(localBase, resultDir);
-//     } else {
-//       // fs.writeFileSync(resultDir + '/' + item[0] + '/' + item, fs.readFileSync(localBase));
-//       fse.writeFileSync(path.join(resultDir, item[0], item), fse.readFileSync(localBase));
-//     }
-//   });
-// };
+  files.forEach((item) => {
+    let localBase = path.join(inputDir, item);
+    let state = fs.statSync(localBase);
+    if (state.isDirectory()) {
+      copyFiles(localBase, outputDir);
+    } else {
+      if (item[0]!='.') fse.writeFileSync(path.join(outputDir, item[0], item), fse.readFileSync(localBase));
+    }
+  });
+};
 
-// createNewDir(resultDir);
-// readDir(targetDir, 0);
-// createDir(dirNames);
-// copyFiles(targetDir, resultDir);
+createNewDir(outputDir);
+readDir(inputDir, 0);
+createDir(dirNames);
+copyFiles(inputDir, outputDir);
