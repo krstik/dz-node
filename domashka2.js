9a76035c2e5110a616d12d164516dfaecf4a94cd
Alexander <Alexander@MacBook-PRO.local>
2018-07-02 18:08:40 +0500

    na proverku 2 dz

---
+++ domashka2.js
@@ -0,0 +1,99 @@
+var fs = require('fs');
+var fse = require('fs-extra');
+var path = require('path');
+var arrayUniq = require('array-uniq');
+var program = require('commander');
+
+program
+  .version('1.0.0')
+  .option('-o, --outdir [name]', 'Directory with sorted files witn name [output]', 'output')
+  .option('-i, --inpdir [name]', 'Directory for sorting files with name [input]', 'input')
+  .parse(process.argv);
+
+
+const inputDir = './' + program.inpdir;
+const outputDir = './' + program.outdir;
+
+async function createNewDir (directoryName)  {
+  // Creating output folder(if exist delete)
+  //if (fse.ensureDirSync(directoryName)) {
+    fs.exists(directoryName, (exists)=>{
+      if (exists) {
+        if (directoryName==outputDir){
+          fse.remove(directoryName, err => {
+            if (err) return console.error(err)
+            console.log('Output exist and removed!')
+          })
+        }
+      }
+  })
+    fs.mkdir(directoryName, (folder) => {
+      return folder
+    })
+  }
+
+function getFolderOutPath (item) {
+    var firstCharName = item.charAt(0).toUpperCase()
+    var folderPath = path.join(outputDir, firstCharName)
+    createNewDir(folderPath)
+    return path.join(folderPath, item)
+  }
+
+// Creating catalog tree
+async function readDir (base, level) {
+  fs.readdir(base, function (err, files) {
+    if (err) {
+      console.log(err.stack)
+    }
+
+    files.forEach((item) => {
+      var localBase = path.join(base, item)
+      if (item[0]!='.'){
+      fs.stat(localBase, (err, fileOrDir) => {
+        if (err) {
+          console.log(err)
+        }
+
+        if (fileOrDir.isDirectory()) {
+          console.log(localBase + ' is directory')
+          readDir(localBase, level + 1)
+        } else {
+          console.log(localBase + ' is file')
+          copyItem(item, localBase)
+        }
+      })
+    }
+    })
+  })
+}
+
+// /making folders in output
+async function createDir (directoriesNames) {
+  var uniqLetter = arrayUniq(directoriesNames);
+
+  uniqLetter.forEach((item, i, arr) => {
+    console.log(path.join(outputDir, item));
+    fs(path.join(outputDir, item));
+  });
+};
+
+// Copying files
+function copyItem (item, localBase) {
+  var outputFile = getFolderOutPath(item)
+  fs.access(outputFile, () => {
+    fs.createReadStream(localBase).pipe(fs.createWriteStream(outputFile))
+  })
+}
+
+
+
+async function main () {
+    try {
+      await createNewDir(outputDir);
+      await readDir(inputDir, 0);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+main()
